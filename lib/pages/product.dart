351b51f29eeebd33c21ca2151640c999ff5ff820
vijay <uvijaysimha@gmail.com>
2019-01-27 11:37:44 +0530

    added product detail page with navigator

---
+++ lib/pages/product.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Product Detail"),
+        ),
+        body: Center(
+          child: Text("Product Detail Page"),
+        ));
+  }
+}
--- lib/products.dart
+++ lib/products.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 
-class Products extends StatelessWidget {
+import './pages/product.dart';
 
+class Products extends StatelessWidget {
   final List<String> items;
 
   Products(this.items);
-  
+
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(items[index])
-                  ],
-                ),
-              );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(items[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Detais'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                    return ProductDetailPage();
+                  }));
+                },
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
-  
+
   Widget _buildProductList() {
     Widget productCard = Center(child: Text("No Products Found"));
-    if(items.length > 0){
+    if (items.length > 0) {
       productCard = ListView.builder(
         itemBuilder: _buildProductItem,
         itemCount: items.length,
